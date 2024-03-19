90caefa663dccc8892f1a35aaf428367c9619e51
meighler <meighler@hidden.com>
2024-03-19 21:48:29 +0200

    Add proper license notices :nerd_face:

--- FCM/FluidGradient/FluidGradientView.swift
+++ FCM/FluidGradient/FluidGradientView.swift
@@ -1,3 +1,25 @@
+ /* 
+ FluidGradientView.swift 
+
+ Copyright (C) 2023, 2024 SparkleChan and SeanIsTethered 
+ Copyright (C) 2024 fridakitten 
+
+ This file is part of FridaCodeManager. 
+
+ FridaCodeManager is free software: you can redistribute it and/or modify 
+ it under the terms of the GNU General Public License as published by 
+ the Free Software Foundation, either version 3 of the License, or 
+ (at your option) any later version. 
+
+ FridaCodeManager is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ GNU General Public License for more details. 
+
+ You should have received a copy of the GNU General Public License 
+ along with FridaCodeManager. If not, see <https://www.gnu.org/licenses/>. 
+ */ 
+    
 //
 //  FluidGradientView.swift
 //  FluidGradientView
