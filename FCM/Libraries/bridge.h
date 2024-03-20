16f29413c300893c94fba1383171c64058b0e132
SeanIsTethered <157828245+fridakitten@users.noreply.github.com>
2024-03-20 16:39:24 +0100

    libhost implementation for deployment version

---
+++ FCM/Libraries/bridge.h
@@ -0,0 +1,2 @@
+#import "libroot/libroot.h"
+#import "libhost/libhost.h"
---
+++ FCM/Libraries/libhost/libhost.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <sys/utsname.h>
+#include <sys/sysctl.h>
+
+NSString *ghost();
+NSString *gosver();
+NSString *gos();
+NSString *gosb();
+NSString *gmodel();
