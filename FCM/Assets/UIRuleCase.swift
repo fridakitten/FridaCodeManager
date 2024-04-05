a86b16cca860c6e77f6f1a1aedcacb81258062bc
SeanIsTethered <157828245+fridakitten@users.noreply.github.com>
2024-04-05 23:53:42 +0200

    adding html to highlighting engine

--- FCM/Assets/UIRuleCase.swift
+++ FCM/Assets/UIRuleCase.swift
@@ -25,9 +25,10 @@ import Foundation
 import UIKit
 
 func grule(_ isaythis: String) -> [HighlightRule] {
-    if isaythis == "swift" {
+    switch(isaythis) {
+        case "swift":
         return [
-         HighlightRule(pattern: try! NSRegularExpression(pattern: "(?<=\\.)\\w+(?=[(])", options: []), formattingRules: [
+        HighlightRule(pattern: try! NSRegularExpression(pattern: "(?<=\\.)\\w+(?=[(])", options: []), formattingRules: [
             TextFormattingRule(key: .foregroundColor, value: UIColor(red: 0, green: 0.6, blue: 0.498, alpha: 1.0))
         ]), HighlightRule(pattern: try! NSRegularExpression(pattern: "\\b(let|var|struct|some|import|private|class|nil|return|func|override)\\b", options: []), formattingRules: [
             TextFormattingRule(key: .foregroundColor, value: UIColor (red: 1.0, green: 0.2, blue: 0.6, alpha: 1.0))
@@ -47,9 +48,9 @@ func grule(_ isaythis: String) -> [HighlightRule] {
             TextFormattingRule(key: .foregroundColor, value: UIColor(red: 1.0, green: 0.5, blue: 0.0, alpha: 1.0))
         ])
     ]
-    } else if ["c", "m", "cpp", "mm"].contains(isaythis) {
+    case "c", "m", "cpp", "mm":
         return [
-         HighlightRule(pattern: try! NSRegularExpression(pattern: "\\b(struct|class|enum|nil|return)\\b", options: []), formattingRules: [
+        HighlightRule(pattern: try! NSRegularExpression(pattern: "\\b(struct|class|enum|nil|return)\\b", options: []), formattingRules: [
             TextFormattingRule(key: .foregroundColor, value: UIColor (red: 1.0, green: 0.2, blue: 0.6, alpha: 1.0))
         ]), HighlightRule(pattern: try! NSRegularExpression(pattern: "@\\w+[^()]", options: []), formattingRules: [
             TextFormattingRule(key: .foregroundColor, value: UIColor(red: 0.7137, green: 0, blue: 1, alpha: 1.0))
@@ -65,6 +66,15 @@ func grule(_ isaythis: String) -> [HighlightRule] {
             TextFormattingRule(key: .foregroundColor, value: UIColor(red: 1.0, green: 0.5, blue: 0.0, alpha: 1.0))
         ])
     ]
-    }
+    case "html":
+    return [
+        HighlightRule(pattern: try! NSRegularExpression(pattern: "<[^>]+>", options: []), formattingRules: [
+            TextFormattingRule(key: .foregroundColor, value: UIColor(red: 0.8, green: 0, blue: 0.1451, alpha: 1.0))
+        ]), HighlightRule(pattern: try! NSRegularExpression(pattern: "(?<!\\/\\/)(\"(.*?)\")", options: []), formattingRules: [
+            TextFormattingRule(key: .foregroundColor, value: UIColor(red: 1.0, green: 0.5, blue: 0.0, alpha: 1.0))
+        ])
+    ]
+    default:
     return []
+    }
 }
