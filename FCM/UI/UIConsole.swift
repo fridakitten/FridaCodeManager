33b2fe687e083c8e53d11d5723d4a1cab734fc29
fridakitten <lolol@polcom.de>
2024-06-28 00:35:46 +0200

    Safe Pointer Management implementation

--- FCM/UI/UIConsole.swift
+++ FCM/UI/UIConsole.swift
@@ -73,8 +73,6 @@ func copyToClipboard(text: String, alert: Bool? = true) {
     if (alert ?? true) {DismissAlert()}
 }
 
-//From https://github.com/Odyssey-Team/Taurine/blob/main/Taurine/app/LogStream.swift
-//Code from Taurine https://github.com/Odyssey-Team/Taurine under BSD 4 License
 class LogStream {
     private(set) var outputString = ""
     private(set) var outputFd: [Int32] = [0, 0]
@@ -82,65 +80,83 @@ class LogStream {
     private let readQueue: DispatchQueue
     private let outputSource: DispatchSourceRead
     private let errorSource: DispatchSourceRead
+    
     init(_ LogItems: Binding<[String.SubSequence]>) {
-        readQueue = DispatchQueue(label: "org.coolstar.sileo.logstream", qos: .userInteractive, attributes: .concurrent, autoreleaseFrequency: .inherit, target: nil)
+        readQueue = DispatchQueue(label: "com.sparklechan.swifty", qos: .userInteractive, attributes: .concurrent, autoreleaseFrequency: .inherit, target: nil)
+        
         pipe(&outputFd)
         pipe(&errFd)
+        
         let origOutput = dup(STDOUT_FILENO)
         let origErr = dup(STDERR_FILENO)
+        
         setvbuf(stdout, nil, _IONBF, 0)
         dup2(outputFd[1], STDOUT_FILENO)
         dup2(errFd[1], STDERR_FILENO)
+        
         outputSource = DispatchSource.makeReadSource(fileDescriptor: outputFd[0], queue: readQueue)
         errorSource = DispatchSource.makeReadSource(fileDescriptor: errFd[0], queue: readQueue)
+        
         outputSource.setCancelHandler {
             close(self.outputFd[0])
             close(self.outputFd[1])
+            dup2(origOutput, STDOUT_FILENO)
+            close(origOutput)
         }
+        
         errorSource.setCancelHandler {
             close(self.errFd[0])
             close(self.errFd[1])
+            dup2(origErr, STDERR_FILENO)
+            close(origErr)
         }
+        
         let bufsiz = Int(BUFSIZ)
-        outputSource.setEventHandler {
-            let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufsiz)
-            defer { buffer.deallocate() }
-            let bytesRead = read(self.outputFd[0], buffer, bufsiz)
-            guard bytesRead > 0 else {
-                if bytesRead == -1 && errno == EAGAIN {
-                    return
-                }
-                self.outputSource.cancel()
-                return
-            }
-            write(origOutput, buffer, bytesRead)
-            let array = Array(UnsafeBufferPointer(start: buffer, count: bytesRead)) + [UInt8(0)]
-            array.withUnsafeBufferPointer { ptr in
-                let str = String(cString: unsafeBitCast(ptr.baseAddress, to: UnsafePointer<CChar>.self))
-                self.outputString.append(str)
-                LogItems.wrappedValue = self.outputString.split(separator: "\n")
-            }
+        
+        outputSource.setEventHandler { [weak self] in
+            self?.handleOutputEvent(bufferSize: bufsiz, originalFd: origOutput, logItems: LogItems)
         }
-        errorSource.setEventHandler {
-            let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufsiz)
-            defer { buffer.deallocate() }
-            let bytesRead = read(self.errFd[0], buffer, bufsiz)
-            guard bytesRead > 0 else {
-                if bytesRead == -1 && errno == EAGAIN {
-                    return
-                }
-                self.errorSource.cancel()
-                return
-            }
-            write(origErr, buffer, bytesRead)
-            let array = Array(UnsafeBufferPointer(start: buffer, count: bytesRead)) + [UInt8(0)]
-            array.withUnsafeBufferPointer { ptr in
-                let str = String(cString: unsafeBitCast(ptr.baseAddress, to: UnsafePointer<CChar>.self))
-                self.outputString.append(str)
-                LogItems.wrappedValue = self.outputString.split(separator: "\n")
-            }
+        
+        errorSource.setEventHandler { [weak self] in
+            self?.handleErrorEvent(bufferSize: bufsiz, originalFd: origErr, logItems: LogItems)
         }
+        
         outputSource.resume()
         errorSource.resume()
+    }
+    
+    private func handleOutputEvent(bufferSize: Int, originalFd: Int32, logItems: Binding<[String.SubSequence]>) {
+        handleEvent(fileDescriptor: outputFd[0], bufferSize: bufferSize, originalFd: originalFd, logItems: logItems)
+    }
+    
+    private func handleErrorEvent(bufferSize: Int, originalFd: Int32, logItems: Binding<[String.SubSequence]>) {
+        handleEvent(fileDescriptor: errFd[0], bufferSize: bufferSize, originalFd: originalFd, logItems: logItems)
+    }
+    
+    private func handleEvent(fileDescriptor: Int32, bufferSize: Int, originalFd: Int32, logItems: Binding<[String.SubSequence]>) {
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
+        defer { buffer.deallocate() }
+        
+        let bytesRead = read(fileDescriptor, buffer, bufferSize)
+        guard bytesRead > 0 else {
+            if bytesRead == -1 && errno == EAGAIN {
+                return
+            }
+            if fileDescriptor == outputFd[0] {
+                outputSource.cancel()
+            } else {
+                errorSource.cancel()
+            }
+            return
+        }
+        
+        write(originalFd, buffer, bytesRead)
+        
+        let array = Array(UnsafeBufferPointer(start: buffer, count: bytesRead)) + [UInt8(0)]
+        array.withUnsafeBufferPointer { ptr in
+            let str = String(cString: unsafeBitCast(ptr.baseAddress, to: UnsafePointer<CChar>.self))
+            self.outputString.append(str)
+            logItems.wrappedValue = self.outputString.split(separator: "\n")
+        }
     }
 }
