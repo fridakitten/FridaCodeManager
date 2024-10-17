2f4df54d058fa7ffc6a77d26c12bfab6f0979abe
fridakitten <lolol@polcom.de>
2024-10-17 15:29:15 +0200

    Chainmaker => improvements for stock iOS

--- Chainmaker/jailed.sh
+++ Chainmaker/jailed.sh
@@ -5,7 +5,7 @@ fi
 mkdir -p .tmp .tmp/procursus .tmp/toolchain .tmp/toolchain/bin .tmp/toolchain/lib
 
 # toolchain packages download #
-packages=("libiosexec1" "libedit0" "libzstd1" "libxar1" "ld64" "libtapi" "libllvm16" "libclang-cpp16" "clang-16" "libclang-common-16-dev" "libssl3")
+packages=("libiosexec1" "libedit0" "libzstd1" "libxar1" "ld64" "libtapi" "libllvm16" "libclang-cpp16" "clang-16" "libclang-common-16-dev" "libssl3" "libncursesw6")
 
 for package in "${packages[@]}"; do
     echo -e "\e[38;5;208mdownloading $package\e[0m"
@@ -33,7 +33,7 @@ CHAIN_RPATH=@loader_path/../lib
 
 ## COPY STAGE ##
 echo -e "\e[38;5;208mcopy stage\e[0m"
-libraries=("libedit.0.dylib" "libiosexec.1.dylib" "libzstd.1.dylib" "libxar.1.dylib" "libtapi.dylib" "libcrypto.3.dylib")
+libraries=("libedit.0.dylib" "libiosexec.1.dylib" "libzstd.1.dylib" "libxar.1.dylib" "libtapi.dylib" "libcrypto.3.dylib" "libncursesw.6.dylib")
 
 for library in "${libraries[@]}"; do
     cp -L $ROOT_LIB/$library $CHAIN_LIB/$library
@@ -55,6 +55,7 @@ for dir in $CHAIN_BIN $CHAIN_LIB; do
         if [ ! -d "$file" ]; then
             # nobody listens to you XD
             install_name_tool -delete_rpath $ROOT_RPATH "$file" > /dev/null 2>&1
+            install_name_tool -delete_rpath /var/jb/usr/lib/llvm-16/lib "$file" > /dev/null 2>&1
             if ! install_name_tool -add_rpath $CHAIN_RPATH "$file" > /dev/null 2>&1; then
                 echo "failed to relink $file"
             else
