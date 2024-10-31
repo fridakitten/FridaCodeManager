d1ab748dcad1695e06fa68a0fe94d5fdab991357
fridakitten <lolol@polcom.de>
2024-10-31 22:28:56 +0100

    App => adding class for the code editor to capture results of the typecheck

--- FCM/UI/UIConsole.swift
+++ FCM/UI/UIConsole.swift
@@ -25,6 +25,31 @@ import SwiftUI
 let LogPipe = Pipe()
 var errorcache: [logstruct] = []
 
+class neolog_extern: NSObject {
+    var LogItems: [LogItem] = []
+    var LogViews: [logstruct] = []
+
+    func start() -> Void {
+        LogPipe.fileHandleForReading.readabilityHandler = { fileHandle in
+            let logData = fileHandle.availableData
+            if !logData.isEmpty, let logString = String(data: logData, encoding: .utf8) {
+                self.LogItems.append(LogItem(Message: logString))
+            }
+        }
+
+        setvbuf(stdout, nil, _IOLBF, 0)
+        setvbuf(stderr, nil, _IOLBF, 0)
+
+        dup2(LogPipe.fileHandleForWriting.fileDescriptor, STDOUT_FILENO)
+        dup2(LogPipe.fileHandleForWriting.fileDescriptor, STDERR_FILENO)
+    }
+
+    func reflushcache() {
+        errorcache = getlog(logitems: LogItems)
+        LogItems = []
+    }
+}
+
 private func getlevelback(_ level: Int) -> Color {
     switch level {
         case 0:
