3f09c43b07998db0c35a02210e9c5d818f145591
darwinx64 <165537498+darwinx64@users.noreply.github.com>
2024-11-06 13:00:17 -0600

    class about to end help

--- FCM/UI/TabBar/Wiki.swift
+++ FCM/UI/TabBar/Wiki.swift
@@ -1,7 +1,42 @@
 import SwiftUI
 
+struct WikiPlaceholderDestination: View {
+    var body: some View {
+        Text("TBD")
+    }
+}
+
 struct WikiView: View {
     var body: some View {
-        Text("To be continued")
+        NavigationView {
+            List {
+                // TODO: Implement destinations, maybe from an API somehow on https://fridacodemanager.github.io?
+                Section {
+                    NavigationLink(destination: WikiPlaceholderDestination()) {
+                        Text("Introduction")
+                    }
+                    NavigationLink(destination: WikiPlaceholderDestination()) {
+                        Text("Features and Limitations")
+                    }
+                    NavigationLink(destination: WikiPlaceholderDestination()) {
+                        Text("Installation")
+                    }
+                } header: {
+                    Label("Prologue", systemImage: "info.circle")
+                }
+                Section {
+                    NavigationLink(destination: WikiPlaceholderDestination()) {
+                        Text("Basic Usage")
+                    }
+                    NavigationLink(destination: WikiPlaceholderDestination()) {
+                        Text("Advanced Usage")
+                    }
+                } header: {
+                    Label("User Guide", systemImage: "text.book.closed")
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationTitle("Wiki")
+        }.navigationViewStyle(.stack)
     }
 }
