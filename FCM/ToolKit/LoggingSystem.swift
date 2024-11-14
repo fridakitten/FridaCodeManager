4e2f5b480d3708bb8e955ca366ba8904268411b4
fridakitten <lolol@polcom.de>
2024-11-14 16:35:51 +0100

    App => Logging System

---
+++ FCM/ToolKit/LoggingSystem.swift
@@ -0,0 +1,99 @@
+//
+//  LogSystem.swift
+//  FloatingDebugLog
+//
+//  Created by fridakitten on 14.11.24.
+//
+
+// LogSystem for FridaCodeManager
+// so its a bit easier :3
+
+import SwiftUI
+import Foundation
+
+// log system
+let mainlogSystem: LogSystem = LogSystem()
+
+class LogSystem: ObservableObject {
+    private let loggingPipe = Pipe()
+    @Published private(set) var log: [String] = []
+    
+    private let logQueue = DispatchQueue(label: "LogSystem.logQueue")
+    
+    init() {
+        loggingPipe.fileHandleForReading.readabilityHandler = { [weak self] fileHandle in
+            guard let self = self else { return }
+            let logData = fileHandle.availableData
+            if !logData.isEmpty, let logString = String(data: logData, encoding: .utf8) {
+                self.addLog(logString)
+            }
+        }
+
+        setvbuf(stdout, nil, _IOLBF, 0)
+        setvbuf(stderr, nil, _IOLBF, 0)
+        
+        dup2(loggingPipe.fileHandleForWriting.fileDescriptor, STDOUT_FILENO)
+        dup2(loggingPipe.fileHandleForWriting.fileDescriptor, STDERR_FILENO)
+    }
+    
+    private func addLog(_ item: String) {
+        DispatchQueue.main.async {
+            self.log.append(item)
+            if self.log.count > 100 {
+                self.log.removeFirst(self.log.count - 100)
+            }
+        }
+    }
+    
+    func dumpLog() {
+        logQueue.async {
+            let path: String = "\(NSTemporaryDirectory())\(UUID())-logdump.txt"
+            let logContent = self.log.joined(separator: "")
+            do {
+                try logContent.write(toFile: path, atomically: true, encoding: .utf8)
+            } catch {
+                print("Failed to write log to \(path): \(error)")
+            }
+            
+            DispatchQueue.main.sync {
+                let fileURL: URL = URL(fileURLWithPath: path)
+                print(fileURL.path)
+                share(url: fileURL)
+            }
+        }
+    }
+    
+    func clearLog() {
+        log = []
+    }
+}
+
+func share(url: URL) -> Void {
+    let activityViewController = UIActivityViewController(activityItems: [url], applicationActivities: nil)
+    activityViewController.modalPresentationStyle = .popover
+    activityViewController.completionWithItemsHandler = { activity, success, items, error in
+        do {
+            try FileManager.default.removeItem(at: url)
+        } catch {
+        }
+    }
+
+    DispatchQueue.main.async {
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+            if let rootViewController = windowScene.windows.first?.rootViewController {
+                if let popoverController = activityViewController.popoverPresentationController {
+                    popoverController.sourceView = rootViewController.view
+                    popoverController.sourceRect = CGRect(x: rootViewController.view.bounds.midX,
+                                                      y: rootViewController.view.bounds.midY,
+                                                      width: 0, height: 0)
+                    popoverController.permittedArrowDirections = []
+                }
+                rootViewController.present(activityViewController, animated: true, completion: nil)
+            } else {
+                print("No root view controller found.")
+            }
+        } else {
+            print("No window scene found.")
+        }
+    }
+}
